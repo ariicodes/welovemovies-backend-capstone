8462f562b50e7a26462411f12bf8d67cff982a06
Ariela Israel <arielaisrael12@gmail.com>
2023-11-05 22:21:54 -0500

    removed commented code

--- src/theaters/theaters.service.js
+++ src/theaters/theaters.service.js
@@ -1,19 +1,5 @@
 const knex = require('../db/connection');
-// const mapProperties = require('../utils/map-properties');
 const reduceProperties = require('../utils/reduce-properties');
-
-// const addMovies = mapProperties({
-// 	movie_id: 'movies.movie_id',
-// 	title: 'movies.title',
-// 	runtime_in_minutes: 'movies.runtime_in_minutes',
-// 	rating: 'movies.rating',
-// 	description: 'movies.description',
-// 	image_url: 'movies.image_url',
-// 	created_at: 'movies.created_at',
-// 	updated_at: 'movies.created_at',
-// 	is_showing: 'movies.is_showing',
-// 	theater_id: 'movies.theater_id',
-// });
 
 const reduceTheatersAndMovies = reduceProperties('theater_id', {
 	movie_id: ['movies', null, 'movie_id'],
@@ -28,14 +14,11 @@ const reduceTheatersAndMovies = reduceProperties('theater_id', {
 });
 
 const list = () => {
-	return (
-		knex('theaters as t')
-			.join('movies_theaters as mt', 't.theater_id', 'mt.theater_id')
-			.join('movies as m', 'm.movie_id', 'mt.movie_id')
-			.select('*')
-			// .then(theaters => theaters.map(theater => addMovies(theater)));
-			.then(reduceTheatersAndMovies)
-	);
+	return knex('theaters as t')
+		.join('movies_theaters as mt', 't.theater_id', 'mt.theater_id')
+		.join('movies as m', 'm.movie_id', 'mt.movie_id')
+		.select('*')
+		.then(reduceTheatersAndMovies);
 };
 
 module.exports = { list };
